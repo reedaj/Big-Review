ee092ad46cce4db9f4516ea0f969c7833739c5c8
Adam Reed <areed@csus.edu>
2021-08-05 18:27:25 -0700

    Initial Commit

---
+++ C-Review/ch5/main.c
@@ -0,0 +1,24 @@
+
+//Standard input output library
+#include <stdio.h>
+//Standard math library
+#include <math.h>
+
+/**	
+*	
+*	Math.h includes the following functions
+*	sqrt ( x ) square root of x
+*	cbrt ( x ) cube root of x
+*	exp ( x )  exponential of x
+*	log ( x )  log base e of x
+*	log10( x ) Log base 10 of x
+*	rabs ( x ) absolute value of x
+*	
+**/
+
+int main(){
+
+	printf( "%2f \n", sqrt(900.0) );
+	
+	return 0;
+}
